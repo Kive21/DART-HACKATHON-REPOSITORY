db0fb49aab010beb77fb88fabba3359fbeb73ba6
Eric Kiverenge <avwengae@gmail.com>
2024-03-17 12:38:43 +0300

    Completed tasks

--- functions.dart
+++ functions.dart
@@ -1,2 +1,29 @@
-## Program 2: Perform Mathematical Operations with Functions
-Write a Dart program that performs two mathematical operations using functions.
+import 'dart:math';
+//## Program 2: Perform Mathematical Operations with Functions
+//Write a Dart program that performs two mathematical operations using functions.
+
+double SphereArea(double radius){
+  return 4 * pi * radius * radius;
+}
+
+double diameter(double radius1){
+  return 2 * radius1;
+}
+
+double SolidCylinderArea(double radius1, double diameter, double height){
+  return (2 * pi * radius1 * radius1) + (pi * diameter * height);
+}
+
+void main() {
+  //Sphere
+  double radius = 7;
+  double area = SphereArea(radius);
+  print("Sphere area is $area");
+
+  //Cylinder
+  double radius1 = 3.5;
+  double height = 10;
+  double d = diameter(radius1);
+  double area2 = SolidCylinderArea(radius1, d, height);
+  print("Cylinder area is $area2");
+}
--- program1..dart
+++ program1..dart
@@ -1,2 +1,20 @@
 ## Program 1: Display Personal Information
 Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+
+class myInformation{
+  String? name;
+  int? age;
+  String? school;
+  String? hobby;
+
+  myInformation(this.name, this.age, this.school, this.hobby);
+
+  void displayInformation(){
+    print("What's up world! $name here, am $age years, proud member of $school and I just love $hobby.");
+  }
+}
+void main(){
+  myInformation myInfo = myInformation("Eric Kiverenge", 23, "PLP Academy", "Algorithm deesigning");
+  
+  myInfo.displayInformation();
+}
